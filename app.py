3388e79f4280599cf4a0360b595cfa219a4eff94
SAKSHI_SHARMA <sakshisharma090199@gmail.com>
2025-10-16 11:19:11 +0530

    Update app to use environment variable for port

--- app.py
+++ app.py
@@ -114,4 +114,5 @@ def metadata(table):
 # Run app
 # ------------------------------
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5001, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
